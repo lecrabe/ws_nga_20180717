281b1c19c1835baf0fc7601ebde2f557b133fdd3
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2018-11-28 14:33:48 +0000

    clip country boundaries

--- scripts/clip_combine.R
+++ scripts/clip_combine.R
@@ -81,6 +81,16 @@ system(sprintf("gdal_calc.py -A %s -B %s -C %s -D %s -E %s  --co COMPRESS=LZW --
                paste0("(A==0)*1+(A>0)*((B==0)*(C==0)*((D>0)*2+(E>0)*5)+(B>0)*3+(C>0)*4)")
 ))
 
+#############################################################
+### CROP TO COUNTRY BOUNDARIES
+system(sprintf("python %s/oft-cutline_crop.py -v %s -i %s -o %s -a %s",
+               scriptdir,
+               paste0(gadm_dir,"gadm_",the_country,"l1.shp"),
+               paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,".tif"),
+               paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,"aoi_.tif"),
+               "OBJECTID"
+))
+
 #################### CREATE A COLOR TABLE FOR THE OUTPUT MAP
 my_classes <- c(0,1,2,3,4,5)
 my_colors  <- col2rgb(c("black","grey","darkgreen","red","orange","lightgreen"))
@@ -100,7 +110,7 @@ write.table(pct,paste0(dd_dir,"color_table.txt"),row.names = F,col.names = F,quo
 ################################################################################
 system(sprintf("(echo %s) | oft-addpct.py %s %s",
                paste0(dd_dir,"color_table.txt"),
-               paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,".tif"),
+               paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,"aoi_.tif"),
                paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,"pct.tif")
 ))
 
@@ -110,6 +120,11 @@ system(sprintf("(echo %s) | oft-addpct.py %s %s",
 system(sprintf("gdal_translate -ot Byte -co COMPRESS=LZW %s %s",
                paste0(dd_dir,"tmp_dd_map_0616_gt",gfc_threshold,"pct.tif"),
                paste0(dd_dir,"dd_map_0616_gt",gfc_threshold,".tif")
+))
+
+
+system(sprintf("rm %s",
+               paste0(dd_dir,"tmp*.tif")
 ))
 
 Sys.time() - time_start
